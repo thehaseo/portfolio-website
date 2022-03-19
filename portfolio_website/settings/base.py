b243674c57750cecef67659913e2582f3f4e5fe2
unknown <gregoriazuaje04@gmail.com>
2022-03-19 11:10:15 -0400

    production

--- portfolio_website/settings/base.py
+++ portfolio_website/settings/base.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('secret_key.txt', 'rt') as key:
-    SECRET_KEY = key
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
