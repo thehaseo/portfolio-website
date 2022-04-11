d1b27e5adad52083925c5c76731f7f703c57312c
unknown <gregoriazuaje04@gmail.com>
2022-04-11 16:31:08 -0400

    fixed responsive navigation bar

--- staticfiles/javascript/script.js
+++
@@ -1,7 +0,0 @@
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-
-
-// post the form and return modal
